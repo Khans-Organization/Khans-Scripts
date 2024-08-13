15e8c109f8803c2871505ed19aeba8c4a7f32cbf
Abdul Moazzim <abdulmoazzam0815@gmail.com>
2024-08-13 06:27:58 -0700

    Strings in js

--- JS/commonjs.js
+++ JS/commonjs.js
@@ -10,6 +10,7 @@ let btn = `<button id="ham2"><span class="material-symbols-outlined">menu</span>
                 <li class="lists"><a class="anchors" href="js6.html">Switch Cases In JavaScript</a></li>
                 <li class="lists"><a class="anchors" href="js7.html">Loops In JavaScript</a></li>
                 <li class="lists"><a class="anchors" href="js8.html">Functions In JavaScript</a></li>
+                <li class="lists"><a class="anchors" href="js9.html">Strings In JavaScript</a></li>
             </ul>
       </div>`
 
