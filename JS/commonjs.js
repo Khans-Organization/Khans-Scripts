7db0d4c3531db369031f624aaa2c0f94c121551e
Abdul Moazzim <abdulmoazzam0815@gmail.com>
2024-08-17 12:17:41 +0500

    Event loop in JS

--- JS/commonjs.js
+++ JS/commonjs.js
@@ -15,6 +15,7 @@ let btn = `<button id="ham2"><span class="material-symbols-outlined">menu</span>
                 <li class="lists"><a class="anchors" href="js11.html">Some methods for Arrays</a></li>
                 <li class="lists"><a class="anchors" href="js12.html">setTimeout and setInterval</a></li>
                 <li class="lists"><a class="anchors" href="js13.html">Scope in JavaScript</a></li>
+                <li class="lists"><a class="anchors" href="js14.html">Event Loop in JavaScript</a></li>
             </ul>
       </div>`
 
