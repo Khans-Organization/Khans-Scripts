f6cb6b60c405048e37ff49e6db462c551ea49757
Abdul Moazzim <abdulmoazzam0815@gmail.com>
2024-08-22 17:55:16 +0500

    Maintaining python folder for my my member

---
+++ PYTHON/commonPython.js
@@ -0,0 +1,17 @@
+let btn = `<button id="ham2"><span class="material-symbols-outlined">menu</span></button>
+      <div class="sidebar behind">
+            <button id="close"><span class="material-symbols-outlined">close</span></button>
+            <ul class="topics">
+                <li class="lists"><a class="anchors" href="js1.html">Introduction to Python</a></li>
+            </ul>
+      </div>`
+
+document.body.insertAdjacentHTML('afterbegin', btn);
+
+document.querySelector('#close').addEventListener('click',()=>{
+    document.body.querySelector('.sidebar').classList.add('behind');
+})
+
+document.querySelector('#ham2').addEventListener('click',()=>{
+    document.body.querySelector('.sidebar').classList.remove('behind');
+})
