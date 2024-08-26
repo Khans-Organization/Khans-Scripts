45fe6033889f62405668d0a92d011cc6dc16f0d6
Abdul Moazzim <abdulmoazzam0815@gmail.com>
2024-08-27 01:19:23 +0500

    Update commonPython.js

--- PYTHON/commonPython.js
+++ PYTHON/commonPython.js
@@ -10,6 +10,7 @@ let btn = `<button id="ham2" class="whiteBack"><span class="material-symbols-out
                 <li class="lists"><a class="anchors" href="py6.html">Loops</a></li>
                 <li class="lists"><a class="anchors" href="py7.html">Functions</a></li>
                 <li class="lists"><a class="anchors" href="py8.html">Strings</a></li>
+                <li class="lists"><a class="anchors" href="py9.html">Lists</a></li>
             </ul>
       </div>`
 
