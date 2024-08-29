b9cb5e2abac361f421fb8a38d600295013175f3f
Nishwa Shaikh <nishwashaikh135@gmail.com>
2024-08-29 20:32:54 +0500

    Scopes done

--- PYTHON/commonPython.js
+++ PYTHON/commonPython.js
@@ -12,7 +12,7 @@ let btn = `<button id="ham2" class="whiteBack"><span class="material-symbols-out
                 <li class="lists"><a class="anchors" href="py8.html">Strings</a></li>
                 <li class="lists"><a class="anchors" href="py9.html">Lists</a></li>
                 <li class="lists"><a class="anchors" href="py10.html">Some methods for Lists</a></li>
-                <li class="lists"><a class="anchors" href="py11.html">Time Module</a></li>
+                <li class="lists"><a class="anchors" href="py11.html">Scope</a></li>
             </ul>
       </div>`
 
