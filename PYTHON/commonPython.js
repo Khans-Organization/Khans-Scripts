15ceab477662b8e0fa97c033ac4de550c6439b08
Abdul Moazzim <abdulmoazzam0815@gmail.com>
2024-08-31 11:59:53 +0500

    Adding close buttons

--- PYTHON/commonPython.js
+++ PYTHON/commonPython.js
@@ -1,6 +1,6 @@
 let btn = `<button id="ham2" class="whiteBack"><span class="material-symbols-outlined">menu</span></button>
       <div class="sidebar behind red">
-            <button id="close" class="red"><span class="material-symbols-outlined">close</span></button>
+            <button id="close1" class="red"><span class="material-symbols-outlined">close</span></button>
             <ul class="topics">
                 <li class="lists"><a class="anchors" href="py1.html">Introduction to Python</a></li>
                 <li class="lists"><a class="anchors" href="py2.html">Declaring Variables</a></li>
@@ -22,7 +22,7 @@ let btn = `<button id="ham2" class="whiteBack"><span class="material-symbols-out
 
 document.body.insertAdjacentHTML('afterbegin', btn);
 
-document.querySelector('#close').addEventListener('click',()=>{
+document.querySelector('#close1').addEventListener('click',()=>{
     document.body.querySelector('.sidebar').classList.add('behind');
 })
 
@@ -34,8 +34,8 @@ document.querySelector('#ham2').addEventListener('click',()=>{
 if (JSON.parse(myStorage.getItem('val')) === 1) {
     document.querySelector('.sidebar').classList.remove('red');
     document.querySelector('.sidebar').classList.add('gray');
-    document.querySelector('#close').classList.remove('red');
-    document.querySelector('#close').classList.add('gray');
+    document.querySelector('#close1').classList.remove('red');
+    document.querySelector('#close1').classList.add('gray');
     document.querySelector('#ham2').classList.remove('whiteBack');
     document.querySelector('#ham2').classList.add('black');
     document.querySelector('#ham2').classList.add('white');
@@ -49,8 +49,8 @@ Array.from(darkMode).forEach((val)=>{
             document.querySelector('#ham2').classList.add('black');
             document.querySelector('.sidebar').classList.remove('red');
             document.querySelector('.sidebar').classList.add('gray');
-            document.querySelector('#close').classList.remove('red');
-            document.querySelector('#close').classList.add('gray');
+            document.querySelector('#close1').classList.remove('red');
+            document.querySelector('#close1').classList.add('gray');
         }
         else {
             document.querySelector('#ham2').classList.remove('white');
@@ -58,8 +58,8 @@ Array.from(darkMode).forEach((val)=>{
             document.querySelector('#ham2').classList.add('whiteBack');
             document.querySelector('.sidebar').classList.remove('gray');
             document.querySelector('.sidebar').classList.add('red');
-            document.querySelector('#close').classList.remove('gray');
-            document.querySelector('#close').classList.add('red');
+            document.querySelector('#close1').classList.remove('gray');
+            document.querySelector('#close1').classList.add('red');
         }
     })
 })
